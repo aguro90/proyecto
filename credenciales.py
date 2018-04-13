bbcabe6d3accb4f7d112501c7b437cd77f84bdc7
root <aldelama96@gmail.com>
2018-04-13 14:41:33 +0200

    Sync

--- credenciales.py
+++
@@ -1,4 +0,0 @@
-consumer_key = '0NLqf3W3RusCKFfk2cj1NAXVl'
-consumer_secret = '2JLKstfcDXpZTpbRFUHk9khe64N9JHDI8eStNhKPGCtF5nnKoB'
-access_token = '273587264-cLW9fWyOmtK0F2yNJFqzjCRmM5tNcGmGhRnp1ruy'
-access_secret = '1Ba4qVilSUeMJIDewagyKKcruwbvaOCi2wBaiQ65egGys'
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-for a in range(10):
-	print(a)
+import requests
+import tweepy
+from tweepy import OAuthHandler
+from credenciales import *
 
-print("<i>hola<i>");
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print ('Error! Failed to get request token.')
+
+print(redirect_url)
+#verifier = request.GET.get('oauth_verifier')
